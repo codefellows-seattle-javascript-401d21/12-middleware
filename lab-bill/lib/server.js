6c3c592bd71064de6349326544a5e34ce113e8c0
billodell <billodell5@gmail.com>
2018-01-30 15:01:31 -0800

    test scaffolding2

--- lab-bill/lib/server.js
+++ lab-bill/lib/server.js
@@ -2,26 +2,28 @@
 
 const express = require('express');
 const errorHandler = require('./error-handler');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router();
 app.use('/api/v1', router);
-
+app.use(cors());
 require('../route/route-note')(router);
 
 app.use('/*', (req, res) => errorHandler(new Error('Path Error. Route not found.'), res));
 
 const server = module.exports = {};
 server.isOn = false;
+server.http = null;
 
 server.start = function(port, callback) {
   if(server.isOn) return callback(new Error('Server running. Cannot start server again.'));
   server.isOn = true;
-  return app.listen(port, callback);
+  server.http = app.listen(port, callback);
 };
 
 server.stop = function(callback) {
   if(!server.isOn) return callback(new Error('Server not running.'));
   server.isOn = false;
-  return app.close(callback);
+  server.http.close();
 };
