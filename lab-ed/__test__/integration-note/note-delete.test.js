1225e9750663a8747be2f29de013d1bad3968795
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-30 21:45:41 -0800

    completed delete testing

--- lab-ed/__test__/integration-note/note-delete.test.js
+++ lab-ed/__test__/integration-note/note-delete.test.js
@@ -7,31 +7,25 @@ require('jest')
 describe('DELETE /api/v1/note', function() {
   this.mockNote = {id: '10', title: 'hello', content: 'hello world'}
 
-  beforeAll(() => server.start(process.env.PORT, (err) => console.log(`Listening on ${process.env.PORT}`)))
+  beforeAll(() => server.start(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`)))
   afterAll(() => server.stop())
 
-  describe('Valid req/res', () => {
+  describe('POST so we can delete', () => {
     beforeAll(() => {
-      return superagent.delete(':4000/api/v1/note/10')
+      return superagent.post(':4000/api/v1/note')
         .send(this.mockNote)
-        .then(res => this.response = res)
-    })
-
-    it('should respond with a status of 204', () => {
-      expect(this.response.status).toBe(204)
-    })
-  })
-  // it('should post a new note with title, content, and _id', () => {
-  //   expect(this.response.body).toHaveProperty('title')
-  //   expect(this.response.body).toHaveProperty('content')
-  //   expect(this.response.body).toHaveProperty('_id')
-  // })
-  // it('should respond with a title of "hello" and content of "hello world"', () => {
-  //   expect(this.response.body.title).toEqual(this.mockNote.title)
-  //   expect(this.response.body.content).toEqual(this.mockNote.content)
-  // })
+        .then(res => this.response = res);
+    });
+    describe('DELETE /api/v1/note', () => {
+      it('should respond with a status of 204', () => {
+        return superagent.delete(`:4000/api/v1/note/${this.response.body._id}`)
+          .then(res => {
+            expect(res.status).toBe(204);
+          });
+      });
+    });
+  });
   
-
   describe('Invalid req/res', () => {
     it('should return a status 404 on bad path', () => {
       return superagent.delete(':4000/api/v1/doesNotExist')
@@ -41,10 +35,5 @@ describe('DELETE /api/v1/note', function() {
           expect(err.response.text).toMatch(/path error/i)
         })
     })
-    // it('should return a status 400 on bad request body', () => {
-    //   return superagent.delete(':4000/api/v1/note')
-    //     .send({})
-    //     .catch(err => expect(err.status).toBe(400))
-    // })
   })
 })
