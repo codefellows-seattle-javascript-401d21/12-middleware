8306e503d18cd0419cda5f25a751bd1e85c6c80c
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-30 21:51:31 -0800

    add get testing

--- lab-ed/__test__/integration-note/note-get.test.js
+++ lab-ed/__test__/integration-note/note-get.test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const server = require('../../lib/server')
+const superagent = require('superagent')
 require('jest')
 
 describe('GET /api/v1/note', function () {
@@ -8,18 +9,30 @@ describe('GET /api/v1/note', function () {
   afterAll(() => server.stop())
 
   describe('Valid req/res', () => {
+    beforeAll(() => {
+      return superagent.get(':4000/api/v1/note')
+        .send(this.mockNote)
+        .then(res => this.response = res)
+    })
+
+    it('should respond with a status of 200', () => {
+      expect(this.response.status).toBe(200)
+    })
   })
+
   describe('Invalid req/res', () => {
-    it('should return true', () => expect(true).toBeTruthy())
+    it('should return a status 404 on bad path', () => {
+      return superagent.get(':4000/api/v1/doesNotExist')
+        .send(this.mockNote)
+        .catch(err => {
+          expect(err.status).toBe(404)
+          expect(err.response.text).toMatch(/path error/i)
+        })
+    })
+    it('should return a status 400 on bad request body', () => {
+      return superagent.get(':4000/api/v1/note')
+        .send({})
+        .catch(err => expect(err.status).toBe(400))
+    })
   })
 })
-
-
-// it('should return a status 404 on bad id parameter', () => {
-//   return superagent.post(':4000/api/v1/note/')
-//     .send(this.mockNote)
-//     .catch(err => {
-//       expect(err.status).toBe(404)
-//       expect(err.response.text).toMatch(/path error/i)
-//     })
-// })
