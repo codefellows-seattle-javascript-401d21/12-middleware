fae864e12f6a420bec2605ef8cbb8078c795030f
billodell <billodell5@gmail.com>
2018-01-30 16:04:13 -0800

    get tests working

--- lab-bill/model/note.js
+++ lab-bill/model/note.js
@@ -4,7 +4,7 @@ const uuid = require('uuid/v4');
 
 module.exports = function Note(name, data) {
   return new Promise((resolve, reject) => {
-    if(!name || !data) return reject(new Error('Cannot create Note. name and data required'));
+    if(!name || !data) return reject(new Error('Validation Error. Cannot create Note. name and data required'));
     this._id = uuid();
     this.name = name;
     this.data = data;
