d9942656e6d68ebbc2571b2dfaf080d083f11643
Koko Kassa <kowserk7@gmail.com>
2018-01-30 18:23:31 -0800

    spelling check

--- lab-koko/lib/error-handler.js
+++ lab-koko/lib/error-handler.js
@@ -6,7 +6,7 @@ module.exports = function(err, res) {
   switch(true) {
   case msg.includes('Validation Error'): 
     return res.status(400).send(`${err.name}: ${err.message}`);
-  case msg.includes('ENONET'): 
+  case msg.includes('ENOENT'): 
     return res.status(404).send(`${err.name}: ${err.message}`);
   case msg.includes('Route not found'):
     return res.status(404).send(`${err.name}: ${err.message}`);
