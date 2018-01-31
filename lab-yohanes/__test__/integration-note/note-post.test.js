6a3d19a705ebf58a5c66181441628f174b896ffe
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-30 22:28:42 -0800

    testing not working on local. only on other laptops for some reason

--- lab-yohanes/__test__/integration-note/note-post.test.js
+++ lab-yohanes/__test__/integration-note/note-post.test.js
@@ -5,17 +5,18 @@ const superagent = require('superagent');
 require('jest');
 
 describe('POST api/v1/note', function () {
+  this.mockNote = { title: 'hello', content: 'hello world' };
+
   beforeAll(() => server.start(process.env.PORT, (err) => console.log(`Listening on ${process.env.PORT}`)));
   afterAll(() => server.stop());
 
-  this.mockNote = {title: 'post', content:'post testing'};
-  beforeAll(() => {
-    return superagent.post(':4000/api/v1/note')
-      .send(this.mockNote)
-      .then(res => this.response = res);
-  });
+  describe('Valid req/res', () => {
+    beforeAll(() => {
+      return superagent.post(':4000/api/v1/note')
+        .send(this.mockNote)
+        .then(res => this.response = res);
+    });
 
-  describe('valid req/re', () => {
     it('should respond with a status of 201', () => {
       expect(this.response.status).toBe(201);
     });
@@ -26,22 +27,19 @@ describe('POST api/v1/note', function () {
       expect(this.response.body).toHaveProperty('_id');
     });
 
-    it('should respond with a book of "hello" and content of "hello world"', () => {
+    it('should respond with a title of "hello" and content of "hello world"', () => {
       expect(this.response.body.title).toEqual(this.mockNote.title);
       expect(this.response.body.content).toEqual(this.mockNote.content);
-
     });
   });
 
-
-
-  describe('invalid path', () => {
+  describe('invalid req/res', () => {
     it('should return a status 404 on bad path', () => {
       return superagent.post(':4000/api/v1/doesNotExist')
         .send(this.mockNote)
         .catch(err => {
           expect(err.status).toBe(404);
-          expect(err.response.text).toMatch(/Path Error/i); //(/path error/ i) regex check
+          expect(err.response.text).toMatch(/Path Error/); //(/path error/ i) regex check
         });
     });
 
@@ -52,4 +50,3 @@ describe('POST api/v1/note', function () {
     });
   });
 });
- 
