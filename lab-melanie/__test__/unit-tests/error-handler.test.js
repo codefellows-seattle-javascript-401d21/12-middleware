880eb4ac339b9b589cb188760a1cf036014d772d
Melanie Downing <sayanything830@gmail.com>
2018-01-30 22:10:34 -0800

    updated err handler

--- lab-melanie/__test__/unit-tests/error-handler.test.js
+++ lab-melanie/__test__/unit-tests/error-handler.test.js
@@ -4,11 +4,11 @@ const errHand = require('../../lib/error-handler');
 require('jest');
 
 describe('error-handler', function() {
-  this.validation = new Error('Validation error: Cannot create note, subject or comment missing');
-  this.res = { status: function(stat){this.statusCode = stat; return this; }, send: function(msg){this.message  = msg; return this;}};
+  this.validation = new Error('Validation error: Cannot create note, title or content missing');
+  this.res = {status: function(stat){this.statusCode = stat; return this; }, send: function(msg){this.message  = msg; return this;}};
 
   this.enoent = new Error('enoent');
-  this.path_error = new Error('path error');
+  this.pathError = new Error('path error');
   this.fail = new Error('fail');
   it('should respond with a status of 400', () => {
     let errRes = errHand(this.validation, this.res);
@@ -19,7 +19,7 @@ describe('error-handler', function() {
     expect(errRes.statusCode).toEqual(404);
   });
   it('should respond with a status of 404', () => {
-    let errRes = errHand(this.path_error, this.res);
+    let errRes = errHand(this.pathError, this.res);
     expect(errRes.statusCode).toEqual(404);
   });
   it('should respond with a status of 500', () => {
