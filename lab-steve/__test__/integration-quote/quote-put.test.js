d19da4b9d7fbad7d7f6c0421a4a4f7bded0d2658
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-04 09:03:19 -0800

    Addd some invalid PUT request tests.

--- lab-steve/__test__/integration-quote/quote-put.test.js
+++ lab-steve/__test__/integration-quote/quote-put.test.js
@@ -18,17 +18,30 @@ describe('Route Testing', () => {
         .then(() => this.mockQuote._id = this.response.body._id);
     });
 
-    describe('Valid Routes/Data', () => {
-      it('Should respond with 204 status', () => {
+    describe('Valid PUT /quote', () => {
+      it('should respond with 204 status', () => {
         this.mockQuote.author = 'Tim';
         return superagent.put(`${endpoint}/${this.mockQuote._id}`)
           .send(this.mockQuote)
           .then(res => expect(res.status).toBe(204));
       });
 
-      it('Should respond with the updated quote', () => {
+      it('should respond with the updated quote', () => {
         return superagent.get(`${endpoint}/${this.mockQuote._id}`)
           .then(res => expect(JSON.parse(res.text).author).toBe('Tim'));
+      });
+    });
+
+    describe('Invalid PUT /quote', () => {
+      it('should respond with 204 status when a bogus path is requested', () => {
+        return superagent.put(`${endpoint}/fakepath`)
+          .send({quote: 'Yowzers', author: 'Mike'})
+          .catch(err => expect(err.status).toBe(404));
+      });
+
+      it('should return a 400 status when no body is sent', () => {
+        return superagent.put(`${endpoint}/${this.mockQuote._id}`)
+          .catch(err => expect(err.status).toBe(400));
       });
     });
   });
