aca50ee82c11b5c408ba057b2378c782412af5ef
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-03 19:27:03 -0800

    Added working 204 PUT test

--- lab-steve/__test__/integration-quote/quote-put.test.js
+++ lab-steve/__test__/integration-quote/quote-put.test.js
@@ -2,28 +2,29 @@
 
 const server = require('../../lib/server');
 const superagent = require('superagent');
-require('jest');
 
-describe('POST /api/v1/note', function () {
-  this.mockQuote = {quote: 'Tis the day', author: 'Steve'};
-  this.endpoint = ':4000/api/v1';
-
-  // Start the server
-  beforeAll(() => server.start(process.env.PORT, (err) => {
-    if (err) {
-      console.error(`Error Starting Server: ${err}`);
-      return;
-    }
-    console.log(`Listening on PORT ${process.env.PORT}`);
-  }));
-  // Stop the server
+describe('Route Testing', () => {
+  let PORT = process.env.PORT;
+  let endpoint = `:${PORT}/api/v1/quote`;
+  this.mockQuote = { quote: 'Over the hills', author: 'Joe' };
+  beforeAll(() => server.start(PORT, () => console.log(`listening on ${PORT}`)));
   afterAll(() => server.stop());
 
-  describe('Valid req/res', () => {
-    it('should return true', () => expect(true).toBeTruthy());
-  });
+  describe('PUT /quote', () => {
+    beforeAll(() => {
+      return superagent.post(endpoint)
+        .send(this.mockQuote)
+        .then(res => this.response = res)
+        .then(() => this.mockQuote._id = this.response.body._id);
+    });
 
-  describe('Invalid req/res', () => {
-    it('should return true', () => expect(true).toBeTruthy());
+    describe('Valid Routes/Data', () => {
+      it('Should respond with 204 status', () => {
+        this.mockQuote.author = 'Tim';
+        return superagent.put(`${endpoint}/${this.mockQuote._id}`)
+          .send(this.mockQuote)
+          .then(res => expect(res.status).toBe(204));
+      });
+    });
   });
 });
