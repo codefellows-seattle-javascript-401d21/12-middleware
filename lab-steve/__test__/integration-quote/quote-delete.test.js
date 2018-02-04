f3e6a4dcc9433228c340c2aea2b1bb166ac79e7b
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-04 09:47:29 -0800

    Got the DELETE tests working

--- lab-steve/__test__/integration-quote/quote-delete.test.js
+++ lab-steve/__test__/integration-quote/quote-delete.test.js
@@ -2,21 +2,30 @@
 
 const server = require('../../lib/server');
 const superagent = require('superagent');
-require('jest');
 
-describe('POST /api/v1/quote', function () {
-  beforeAll(() => server.start(process.env.PORT, (err) => {
-    if (err)
-      console.error(`Error Starting Server: ${err}`);
-    console.log(`Listening on ${process.env.PORT}`);
-  }));
+describe('DELETE /api/v1/quote', () => {
+  let PORT = process.env.PORT;
+  let endpoint = `:${PORT}/api/v1/quote`;
+  this.mockQuote = {quote: 'Top of the morning', author: 'Sandy'};
+  beforeAll(() => server.start(PORT, () => console.log(`listening on ${PORT}`)));
   afterAll(() => server.stop());
-
-  describe('Valid req/res', () => {
-    it('should return true', () => expect(true).toBeTruthy());
+  beforeAll(() => {
+    return superagent.post(endpoint)
+      .send(this.mockQuote)
+      .then(res => this.response = res);
   });
 
-  describe('Invalid req/res', () => {
-    it('should return true', () => expect(true).toBeTruthy());
+  describe('Valid', () => {
+    it('should respond 204 status on successful deletion', () => {
+      return superagent.delete(`${endpoint}/${this.response.body._id}`)
+        .then(res => expect(res.status).toBe(204));
+    });
+  });
+
+  describe('Invalid', () => {
+    it('should respond with a 404 when a fake path was requested', () => {
+      return superagent.delete(`${endpoint}/fakepath`)
+        .catch(err => expect(err.status).toBe(404));
+    });
   });
 });
