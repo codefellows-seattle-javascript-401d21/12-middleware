41c7c0e4c8bef7e180df5306dc5c62612d202557
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-02-09 11:59:13 -0800

    add put tests

--- lab-ed/__test__/integration-note/note-put.test.js
+++ lab-ed/__test__/integration-note/note-put.test.js
@@ -10,6 +10,31 @@ describe('PUT /api/v1/note', function() {
   beforeAll(() => server.start(process.env.PORT, (err) => console.log(`Listening on ${process.env.PORT}`)))
   afterAll(() => server.stop())
 
+  describe('Valid req/res', () => {
+  
+    beforeAll(() => {
+      return superagent.post(':4000/api/v1/note')
+        .send(this.mockNote)
+        .then(res => {
+          this.response = res
+        })
+    })
+    beforeAll(() => {
+      return superagent.put(`:4000/api/v1/note/${this.response._id}`)
+        .send({title: 'foo', content: 'bar'})
+        .then(res => this.response = res)
+    })
+    it('should return a status code 204 when complete', () => {
+      expect(this.response.status).toBe(204)
+    })
+    it('should have a new title', () => {
+      expect(this.response.title).not.toBe('foo')
+    })
+    it('should be the same id with new title', () => {
+      expect(this.response._id).toBe(this.mockNote._id)
+    })
+  })
+
   describe('Invalid req/res', () => {
     it('should return a status 404 on bad path', () => {
       return superagent.put(':4000/api/v1/doesNotExist')
