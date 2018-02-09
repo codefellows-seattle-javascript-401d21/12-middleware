504e8282f823d671e9efdc0e19f26684f26dfe3e
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-02-09 12:25:27 -0800

    finish tests

--- lab-ed/__test__/unit-tests/storage.test.js
+++ lab-ed/__test__/unit-tests/storage.test.js
@@ -1,7 +1,36 @@
 'use strict'
 
-require('jest')
+let storage = require('../../lib/storage')
 
-describe('testing dummy', function () {
-  it('should return true', () => expect(true).toBeTruthy())
+let newNote = {title: 'new', content: 'note', _id:  'abababab-abab-abab-abab-abababababab'}
+let update = {title: 'update', content: 'new note', _id:  'abababab-abab-abab-abab-abababababab'}
+
+describe('Storage Module', function() {
+  describe('#create', () => {
+    it('should create a note object', () => {
+      return storage.create('note', newNote)
+        .then(note => expect(note).toBeInstanceOf(Object))
+    })
+  })
+  describe('#fetchOne', () => {
+    it('should return a note object', () => {
+      return storage.fetchOne('note', newNote._id)
+        .then(note => {
+          note = JSON.parse(note.toString())
+          expect(note.content).toBe('note')
+        })
+    })
+  })
+  describe('#fetchAll', () => {
+    it('should return array of ids', () => {
+      return storage.fetchAll('note')
+        .then(ids => expect(Array.isArray(ids)).toBeTruthy())
+    })
+  })
+  describe('#update', () => {
+    it('should return an updated object', () => {
+      return storage.update('note', newNote._id, update)
+        .then(note => expect(note.title).toBe('update'))
+    })
+  })
 })
